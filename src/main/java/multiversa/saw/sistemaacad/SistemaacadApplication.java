ac05e40221e0fb29f08023a07040890667567dda
mateus-gondev <mateus.gondev@gmail.com>
2024-11-19 23:11:12 -0300

    primeiro commit

---
+++ src/main/java/multiversa/saw/sistemaacad/SistemaacadApplication.java
@@ -0,0 +1,15 @@
+package multiversa.saw.sistemaacad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaacadApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SistemaacadApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/multiversa/saw/sistemaacad/SistemaacadApplicationTests.java
@@ -0,0 +1,13 @@
+package multiversa.saw.sistemaacad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaacadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
